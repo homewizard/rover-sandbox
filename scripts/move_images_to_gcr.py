6326a0c9fddc1004ddb738c18afef34f5e6c700f
Rover van der Noort <r.vandernoort@homewizard.com>
2024-08-02 14:43:55 +0200

    update script move_images_to_gcr to include dry-run and test list
    Signed-off-by: Rover van der Noort <r.vandernoort@homewizard.com>

--- scripts/move_images_to_gcr.py
+++ scripts/move_images_to_gcr.py
@@ -1,26 +1,41 @@
 import subprocess
+import argparse
 
-def execute_commands_from_file(file_path):
+parser = argparse.ArgumentParser(
+    description="Move all images from 'image_list.txt' to gcr.io/hw-cloud-service using gcrane.")
+parser.add_argument('-d', '--dry', 
+                    action="store_true",
+                    help="run process without executing gcrane commands")
+parser.add_argument('-t', '--test',
+                    action="store_true",
+                    help="run process with test data in 'images_test_list.txt'")
+
+def execute_commands_from_file(file_path, args):
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
+            print(f"File read")
 
-        print(f"File read")
-        for line in lines:
-            image = line.strip()
-            if image:  # Make sure it's not an empty line
-                try:
-                    # Execute the command
-                    new_image = 'gcr.io/hw-cloud-service/' + "/".join(image.split("/")[1:])
-                    command = f'gcrane copy {image} {new_image}'
-                    result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-                    print(f"Result {result}")
-                    print(f"Command '{command}' executed successfully.")
-                    print(f"Output:\n{result.stdout}")
-                except subprocess.CalledProcessError as e:
-                    print(f"Command '{command}' failed with error:\n{e.stderr}")
-                except Exception as e:
-                    print(f"An error occurred while executing command '{command}': {e}")
+            if args.dry:
+                print("Dry-run only returning commands and not executing them:")
+            
+            for line in lines:
+                image = line.strip()
+                if image:  # Make sure it's not an empty line
+                    try:
+                        # Execute the command
+                        new_image = 'gcr.io/hw-cloud-service/' + "/".join(image.split("/")[1:])
+                        command = f'gcrane copy {image} {new_image}'
+                        if args.dry:
+                            print(f"Command '{command}'")
+                        else:
+                            result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+                            print(f"Command '{command}' executed successfully.")
+                            print(f"Output: {result.stderr}\n{result.stdout}")
+                    except subprocess.CalledProcessError as e:
+                        print(f"Command '{command}' failed with error:\n{e.stderr}")
+                    except Exception as e:
+                        print(f"An error occurred while executing command '{command}': {e}")
 
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
@@ -28,5 +43,11 @@ def execute_commands_from_file(file_path):
         print(f"An error occurred while reading the file '{file_path}': {e}")
 
 if __name__ == "__main__":
-    file_path = './images_list.txt'  # Replace this with your file path
-    execute_commands_from_file(file_path)
+    args = parser.parse_args()
+    if args.test:
+        print("Executing test file")
+        file_path = './images_test_list.txt'
+    else: 
+        print("Executing prod file")
+        file_path = './images_list.txt'
+    execute_commands_from_file(file_path, args)
